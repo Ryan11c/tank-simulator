96c9bfd6c1514b25d4350fd9252c5d75a2f3d841
Ryan Choi <ryan04122005@gmail.com>
2025-09-11 14:53:10 -0700

    Finish destructible header file

--- Destructible.h
+++ Destructible.h
@@ -5,4 +5,16 @@
 class Destructible : public Sprite {
 public:
     Destructible(Vector2D setPos, int setHealthMax = 1, int setExperienceGiven = 1);
+    void update(float dT);
+	void draw(RendererGL* rendererGL, int tileSize, Vector2D posCamera);
+	void drawShadow(RendererGL* rendererGL, int tileSize, Vector2D posCamera);
+	bool isAlive();
+	void removeHealth(int amount);
+	int getExperienceGiven();
+
+private:
+	int healthMax;
+	int healthCurrent;
+	int experienceGiven;
+	float speedAngular = MathAddon::angleDegToRad(45.0f);
 };
